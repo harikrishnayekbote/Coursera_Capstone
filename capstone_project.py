a2c0e5e1e289c7027f3450c269dbfc19d71c9372
harikrishnayekbote <63159440+harikrishnayekbote@users.noreply.github.com>
2020-04-05 16:32:01 +0530

    Add files via upload

---
+++ capstone_project.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project
+# 
+
+# #### Created by Harikrishna Yekbote 
+# ---
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
